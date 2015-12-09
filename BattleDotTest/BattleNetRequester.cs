3e6c76da013f80a1d498b513981bea76cb00858b
NickolasAcosta <NickAcosta@outlook.com>
2015-12-08 18:00:26 -0600

    init 2

--- BattleDotTest/BattleNetRequester.cs
+++ BattleDotTest/BattleNetRequester.cs
@@ -1,69 +1,67 @@
-﻿using System;
-using System.Threading;
-using System.Diagnostics;
-using System.Configuration;
-using QueueDodge;
-using QueueDodge.Integrations;
-using BattleDotNet;
-using Microsoft.AspNet.SignalR.Client;
+﻿//using System;
+//using System.Threading;
+//using System.Diagnostics;
+//using System.Configuration;
+//using BattleDotNet;
+//using Microsoft.AspNet.SignalR.Client;
 
 namespace BattleDotTest
 {
     public class BattleNetRequester
     {
-        private static LeaderboardIntegrationService battleNetLeaderboardService;
+        //  private static LeaderboardIntegrationService battleNetLeaderboardService;
 
         public static void Main(string[] args)
         {
 
-            battleNetLeaderboardService = new LeaderboardIntegrationService(new QueueDodgeDB(), "heuemgj94eyv484cekut2a82d6crnskm");
+            //            battleNetLeaderboardService = new LeaderboardIntegrationService(new QueueDodgeDB(), "heuemgj94eyv484cekut2a82d6crnskm");
 
-                string bracket = args[0];
-                Region region = (Region)Enum.Parse(typeof(Region), args[1], true);
-                Locale locale;
-                Console.WriteLine(bracket);
-                Console.WriteLine(region);
+            //                string bracket = args[0];
+            //                Region region = (Region)Enum.Parse(typeof(Region), args[1], true);
+            //                Locale locale;
+            //                Console.WriteLine(bracket);
+            //                Console.WriteLine(region);
 
-                switch (region.ToString())
-                {
-                    case "us":
-                        locale = Locale.en_us;
-                        break;
-                    case "eu":
-                        locale = Locale.en_gb;
-                        break;
+            //                switch (region.ToString())
+            //                {
+            //                    case "us":
+            //                        locale = Locale.en_us;
+            //                        break;
+            //                    case "eu":
+            //                        locale = Locale.en_gb;
+            //                        break;
 
-                    default:
-                        locale = Locale.en_us;
-                        throw new Exception("Region or locale not supported.");
-                }
-
-
-            do
-            {
+            //                    default:
+            //                        locale = Locale.en_us;
+            //                        throw new Exception("Region or locale not supported.");
+            //                }
 
 
-                var hub = ConfigurationManager.AppSettings["HubAddress"];
+            //            do
+            //            {
 
-                // var connection = new HubConnection("http://localhost:51906/");
-                var connection = new HubConnection(hub);
-                IHubProxy myHub = connection.CreateHubProxy("LeaderboardHub");
 
-                connection.Start().Wait(); // not sure if you need this if you are simply posting to the hub
+            //                var hub = ConfigurationManager.AppSettings["HubAddress"];
 
-                myHub.Invoke("LeaderboardRequestStarted", bracket, (int)region, DateTime.Now);
+            //                // var connection = new HubConnection("http://localhost:51906/");
+            //                var connection = new HubConnection(hub);
+            //                IHubProxy myHub = connection.CreateHubProxy("LeaderboardHub");
 
-                var request = battleNetLeaderboardService.GetLeaderboard(bracket, ".api.battle.net/", region, Game.wow, locale);
+            //                connection.Start().Wait(); // not sure if you need this if you are simply posting to the hub
 
-                Console.WriteLine(bracket + " Leaderboard time: {0} at " + DateTime.Now, request.Duration);
+            //                myHub.Invoke("LeaderboardRequestStarted", bracket, (int)region, DateTime.Now);
 
-                myHub.Invoke("LeaderboardRequestComplete", bracket, (int)region, DateTime.Now);
+            //                var request = battleNetLeaderboardService.GetLeaderboard(bracket, ".api.battle.net/", region, Game.wow, locale);
 
-                   Thread.Sleep(30000);
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
-                GC.Collect();
-            } while (1 == 1);
+            //                Console.WriteLine(bracket + " Leaderboard time: {0} at " + DateTime.Now, request.Duration);
+
+            //                myHub.Invoke("LeaderboardRequestComplete", bracket, (int)region, DateTime.Now);
+
+            //                   Thread.Sleep(30000);
+            //                GC.Collect();
+            //                GC.WaitForPendingFinalizers();
+            //                GC.Collect();
+            //            } while (1 == 1);
         }
     }
 }
