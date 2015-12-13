f2c89f4f913663d467cbc99a2f86a81befbf2aec
srmeyer1989 <srmeyer1989@gmail.com>
2015-12-12 20:26:35 -0600

    Recipe API Added

---
+++ BattleDotNet/Recipe/Recipe.cs
@@ -0,0 +1,11 @@
+﻿namespace BattleDotNet.Recipe
+{
+
+    public class Recipe
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string profession { get; set; }
+        public string icon { get; set; }
+    }
+}
