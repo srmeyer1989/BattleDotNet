b862544e42bf08e3ac03cdd7aaa1e1f90820662b
srmeyer1989 <srmeyer1989@gmail.com>
2015-12-12 20:18:22 -0600

    Added Quest API

---
+++ BattleDotNet/Quest/Quest.cs
@@ -0,0 +1,12 @@
+﻿namespace BattleDotNet.Quest
+{
+    public class Quest
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public int reqLevel { get; set; }
+        public int suggestedPartyMembers { get; set; }
+        public string category { get; set; }
+        public int level { get; set; }
+    }
+}
---
+++ BattleDotNet/Quest/QuestEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleDotNet.Quest
+{
+    public class QuestEndpoint : IEndpoint
+    {
+        public IList<IEndpointParameter> Parameters { get; set; }
+
+        public QuestEndpoint(string questID, Locale locale, string apiKey)
+        {
+            Parameters = new List<IEndpointParameter>();
+
+            Parameters.Add(new EndpointParameter("quest", EndpointParameterType.Resource));
+            Parameters.Add(new EndpointParameter(questID, EndpointParameterType.Resource));
+            Parameters.Add(new EndpointParameter("locale", locale.ToString(), EndpointParameterType.Query));
+            Parameters.Add(new EndpointParameter("apikey", apiKey, EndpointParameterType.Query));
+        }
+
+
+    }
+}
